248ae86a25fd73c2b6300ee8bacfd9a72931e01d
JMustang <eecfredes@gmail.com>
2021-02-24 01:12:00 -0300

    feat: segunda aula do NLW, criando banco de dados sqlite3

--- API/src/comandos.md
+++ API/src/comandos.md
@@ -1,1 +1,11 @@
 #rumoaoproximonivel
+#jornadainfinita
+
+yarn add express
+yarn add typescript
+yarn add ts-node-dev
+yarn add @types/express
+yarn add typeorm reflect-metadata
+yarn add sqlite3
+yarn add uuid
+yarn add @types/uuid -D
--- API/src/server.ts
+++ API/src/server.ts
@@ -1,26 +1,12 @@
+import 'reflect-metadata'
 import express from 'express';
-const PORT = 3000;
+import './database'
+import { router } from './routes';
 
+const PORT = 3000;
 const app = express();
 
-/**
- * GET => Faz Buscas
- * POST => Salva
- * PUT => Faz Alteracos
- * DELETE => Deleta
- * PATCH => Faz ateracoes Especificas
- */
-
-//http://localhost:3000/
- app.get("/", (req, res) => {
-     return res.json({message: `primeira aula da <NWL>`})
- });
-
-// primeiro parametro => rota (recuso API)
-// segundo parametro => request,response
- app.post('/', (req, res) => {
-     // Recebeu os dados para salvar
-     return res.json({message: 'Os dados foram salvos com sucesso!'})
- })
+app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () => console.log(`Server Running at: http://localhost:${PORT}`)); 
