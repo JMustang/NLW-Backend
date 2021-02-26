e72138a9f487a5398274670e78d485cb988970fc
JMustang <eecfredes@gmail.com>
2021-02-26 02:29:34 -0300

    feat: quarta aula do NWL, envio de email

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,6 +1,3 @@
 import { app } from "./app";
-const PORT = 3000;
 
-app.listen(PORT, () =>
-  console.log(`Server Running at: http://localhost:${PORT}`)
-);
+app.listen(3000, () => console.log(`Server Running at: http://localhost:3000`));
